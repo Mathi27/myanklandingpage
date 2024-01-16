b2d45a2694a3bc9ed722cb2aca238d58fc5e99ee
mathi27 <mathiyuvarajan.dev@gmail.com>
2024-01-16 21:49:45 +0530

    cta

--- src/Pages/homepage.jsx
+++ src/Pages/homepage.jsx
@@ -5,6 +5,7 @@ function Home(){
         <p className="intro-text">
           Gear up for the future! 🚀 Currently in R&D mode, crafting the coolest innovations for our next big adventure. Stay tuned, the hype is real.
         </p>
+        <button className='cta-btn'>Launching Soon</button>
         <img className="founders-image" src={gopal} alt='Founders' />
       </div>
     )
